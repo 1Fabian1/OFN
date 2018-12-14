dd8f1870c7ee3ba87515d9f659d76c02b4acc389
1Fabian1 <34246069+1Fabian1@users.noreply.github.com>
2018-12-14 08:36:07 +0100

    number class

---
+++ OFN/OFN/FuzzyNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OFN
+{
+    class FuzzyNumber
+    {
+        private double pos1;
+        private double pos2;
+        private double pos3;
+        private double pos4;
+
+        public FuzzyNumber(double pos1, double pos2, double pos3, double pos4)
+        {
+            this.pos1 = pos1;
+            this.pos2 = pos2;
+            this.pos3 = pos3;
+            this.pos4 = pos4;
+        }
+
+        public FuzzyNumber(double pos1, double pos2, double pos4)
+        {
+            this.pos1 = pos1;
+            this.pos2 = pos2;
+            this.pos3 = pos2;
+            this.pos4 = pos4;
+        }
+
+        public double Pos1 { get => pos1; set => pos1 = value; }
+        public double Pos2 { get => pos2; set => pos2 = value; }
+        public double Pos3 { get => pos3; set => pos3 = value; }
+        public double Pos4 { get => pos4; set => pos4 = value; }
+    }
+}
