b67f649ec1516fe74f90044f259f1d664e38ee7f
emskypl <mateusz.emsky@gmail.com>
2018-12-21 18:43:08 +0100

    Polynomial UI, PolynomialAlgebra(addAndSubstract), Correct Polynomial.ToString(), Correct Polynomial Logic

--- OFN/OFN/Polynomial.cs
+++ OFN/OFN/Polynomial.cs
@@ -3,68 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.UI.Xaml.Controls;
-using System.IO;
-using Windows.UI.Popups;
-using Windows.UI.Xaml;
+
 namespace OFN
 {
     class Polynomial
     {
-        
-        List<UIElement> listOfTextBoxes = new List<UIElement>();
-        public async void CreateTextBoxes(TextBox textBoxNumberTextBoxes, Grid polynomialGrid)
-        {           
-            int numberOfTextBoxes = 0;
-            var dialog = new MessageDialog("Stopień wielomianu musi być liczbą całkowitą i nie większą niż 10.");
+        private double freeValue;
+        private double valueX;
+        private double valueX2;
+        private double valueX3;
+        private double valueX4;
+        private double valueX5;
+        private double valueX6;
+        private double valueX7;
+        private double valueX8;
+        private double valueX9;
+        private double valueX10;
 
-            foreach (var children in polynomialGrid.Children)
+        public Polynomial()
+        {
+            this.freeValue = 0;
+            this.valueX = 0;
+            this.valueX2 = 0;
+            this.valueX3 = 0;
+            this.valueX4 = 0;
+            this.valueX5 = 0;
+            this.valueX6 = 0;
+            this.valueX7 = 0;
+            this.valueX8 = 0;
+            this.valueX9 = 0;
+            this.valueX10 = 0;
+        }
+
+        public Polynomial(double freeValue, double valueX, double valueX2, double valueX3, double valueX4, double valueX5, double valueX6, double valueX7, double valueX8, double valueX9, double valueX10)
+        {
+            this.freeValue = freeValue;
+            this.valueX = valueX;
+            this.valueX2 = valueX2;
+            this.valueX3 = valueX3;
+            this.valueX4 = valueX4;
+            this.valueX5 = valueX5;
+            this.valueX6 = valueX6;
+            this.valueX7 = valueX7;
+            this.valueX8 = valueX8;
+            this.valueX9 = valueX9;
+            this.valueX10 = valueX10;
+        }
+
+        public double FreeValue { get => freeValue; set => freeValue = value; }
+        public double ValueX { get => valueX; set => valueX = value; }
+        public double ValueX2 { get => valueX2; set => valueX2 = value; }
+        public double ValueX3 { get => valueX3; set => valueX3 = value; }
+        public double ValueX4 { get => valueX4; set => valueX4 = value; }
+        public double ValueX5 { get => valueX5; set => valueX5 = value; }
+        public double ValueX6 { get => valueX6; set => valueX6 = value; }
+        public double ValueX7 { get => valueX7; set => valueX7 = value; }
+        public double ValueX8 { get => valueX8; set => valueX8 = value; }
+        public double ValueX9 { get => valueX9; set => valueX9 = value; }
+        public double ValueX10 { get => valueX10; set => valueX10 = value; }
+
+        public override string ToString()
+        {
+            String resultString = "";
+            if (!valueX10.Equals(0)) resultString = resultString + valueX10.ToString() + "x\xB9" + "\x2070";
+            if (valueX9 > 0) resultString = resultString + "+";
+            if (!valueX9.Equals(0)) resultString = resultString + valueX9.ToString() + "x\x2079";
+            if (valueX8 > 0) resultString = resultString + "+";
+            if (!valueX8.Equals(0)) resultString = resultString + valueX8.ToString() + "x\x2078";
+            if (valueX7 > 0) resultString = resultString + "+";
+            if (!valueX7.Equals(0)) resultString = resultString + valueX7.ToString() + "x\x2077";
+            if (valueX6 > 0) resultString = resultString + "+";
+            if (!valueX6.Equals(0)) resultString = resultString + valueX6.ToString() + "x\x2076";
+            if (valueX5 > 0) resultString = resultString + "+";
+            if (!valueX5.Equals(0)) resultString = resultString + valueX5.ToString() + "x\x2075";
+            if (valueX4 > 0) resultString = resultString + "+";
+            if (!valueX4.Equals(0)) resultString = resultString + valueX4.ToString() + "x\x2074";
+            if (valueX3 > 0) resultString = resultString + "+";
+            if (!valueX3.Equals(0)) resultString = resultString + valueX3.ToString() + "x\xB3";
+            if (valueX2 > 0) resultString = resultString + "+";
+            if (!valueX2.Equals(0)) resultString = resultString + valueX2.ToString() + "x\xB2";
+            if (valueX > 0) resultString = resultString + "+";
+            if (!valueX.Equals(0)) resultString = resultString + valueX.ToString()   + "x";
+            if (freeValue.Equals(0)) { }
+            else if(freeValue > 0)
             {
-                listOfTextBoxes.Add(children);
-                
+                resultString = resultString + "+";
+                resultString = resultString + freeValue.ToString();
+            } else if(freeValue < 0) 
+            {
+                resultString = resultString + freeValue.ToString();
             }
 
-            for (int i = 0; i <= listOfTextBoxes.Count() - 3; i++)
+            StringBuilder stringBuilder = new StringBuilder(resultString);
+            if (stringBuilder[0].Equals('+'))
             {
-                listOfTextBoxes[i + 2].Visibility = Visibility.Collapsed;
+                stringBuilder.Remove(0, 1);
             }
-
-            try
-            {
-                numberOfTextBoxes = Int32.Parse(textBoxNumberTextBoxes.Text);
-            }
-            catch (Exception e)
-            {
-                if (!textBoxNumberTextBoxes.Text.Equals(""))
-                {
-                    
-                    await dialog.ShowAsync();
-                    return;
-                }
-                return;
-            }
-            
-
-            if (numberOfTextBoxes > 10)
-            {
-                dialog = new MessageDialog("Podaj liczbę mniejszą lub równą 10.");
-                await dialog.ShowAsync();
-            }
-            else
-            {
-                for(int i=0; i <= numberOfTextBoxes; i++)
-                {
-                    if(numberOfTextBoxes <= listOfTextBoxes.Count() - 2)
-                        listOfTextBoxes[i + 1].Visibility = Visibility.Visible;                 
-                }
-            }
-            listOfTextBoxes.Clear();
+            resultString = stringBuilder.ToString();
 
 
-            // TODO -----------> Algorytm liczacy up&down podanych wielomianow
-            // test.Text = polynomialGrid.Children.Count().ToString();
-
-
+            return resultString;
+         
         }
     }
+
+    
 }
---
+++ OFN/OFN/PolynomialTextBoxes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using System.IO;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+namespace OFN
+{
+    class PolynomialTextBoes
+    {
+        
+        List<TextBox> listOfTextBoxes = new List<TextBox>();
+        public async void CreateTextBoxes(TextBox textBoxNumberTextBoxes, Grid polynomialGrid)
+        {           
+            int numberOfTextBoxes = 0;
+            var dialog = new MessageDialog("Stopień wielomianu musi być liczbą całkowitą i nie większą niż 10.");
+
+            foreach (TextBox children in polynomialGrid.Children)
+            {
+                listOfTextBoxes.Add(children);
+                
+            }
+            System.Diagnostics.Debug.Write(listOfTextBoxes.Count().ToString());
+            for (int i = 2; i <= listOfTextBoxes.Count() - 1; i++)
+            {
+                listOfTextBoxes[i].Visibility = Visibility.Collapsed;
+            }
+
+            try
+            {
+                numberOfTextBoxes = Int32.Parse(textBoxNumberTextBoxes.Text);
+            }
+            catch (Exception e)
+            {
+                if (!textBoxNumberTextBoxes.Text.Equals(""))
+                {
+                    
+                    await dialog.ShowAsync();
+                    return;
+                }
+                return;            
+            }
+            
+
+            if (numberOfTextBoxes > 10)
+            {
+                dialog = new MessageDialog("Podaj liczbę mniejszą lub równą 10.");
+                await dialog.ShowAsync();
+            }
+            else
+            {
+                for(int i=1; i <= numberOfTextBoxes; i++)
+                {
+                    if(numberOfTextBoxes <= listOfTextBoxes.Count())
+                        listOfTextBoxes[i+1].Visibility = Visibility.Visible;                 
+                }
+                
+                for(int i = numberOfTextBoxes + 3; i <= listOfTextBoxes.Count(); i++)
+                {
+                    listOfTextBoxes[i - 1].Text = "";
+                }
+                
+            }
+            listOfTextBoxes.Clear();
+                                 
+           
+
+
+        }
+    }
+}
